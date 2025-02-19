eb78aaa77a34ba581c80be9c9d659b2c73a8c947
Will Urban <mica@talisman.xyz>
2025-02-19 14:43:53 +0700

    chore: setup loading .env and gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import { config } from "dotenv";
+
 import { ApiPromise, WsProvider } from "@polkadot/api";
+
+// Load environment variables
+config();
+
+const endpoint = `wss://bittensor-finney.api.onfinality.io/ws?apikey=${process.env.ON_FINALITY_API_KEY}`;
 
 async function main() {
   // Connect to a Polkadot node
-  const wsProvider = new WsProvider("wss://rpc.polkadot.io");
+  const wsProvider = new WsProvider(endpoint);
   const api = await ApiPromise.create({ provider: wsProvider });
 
   // Retrieve and display chain information
